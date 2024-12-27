587f0d7b91cb9a225db028464151c93bd17bef60
Keijiro Takahashi <keijiro@gmail.com>
2024-12-27 16:43:41 +0900

    UI

--- Assets/Scripts/ResetSwitch.cs
+++ Assets/Scripts/ResetSwitch.cs
@@ -1,6 +1,6 @@
 using UnityEngine;
-using UnityEngine.SceneManagement;
 using UnityEngine.InputSystem;
+using UnityEngine.UIElements;
 
 public sealed class ResetSwitch : MonoBehaviour
 {
@@ -8,19 +8,35 @@ public sealed class ResetSwitch : MonoBehaviour
 
     [SerializeField] GameObject _prefab = null;
     [SerializeField] InputAction _trigger = null;
+    [SerializeField] string _uiButtonName = "give-up-button";
+
+    Button _button;
 
     void OnEnable()
-      => _trigger.Enable();
+    {
+        _button = FindFirstObjectByType<UIDocument>()
+          .rootVisualElement.Q<Button>(_uiButtonName);
+
+        _trigger.Enable();
+
+        _button.clicked += ResetPlayer;
+    }
 
     void OnDisable()
-      => _trigger.Disable();
+    {
+        _trigger.Disable();
+
+        _button.clicked -= ResetPlayer;
+    }
 
     void Update()
     {
-        if (_trigger.WasPerformedThisFrame())
-        {
-            Destroy(GameObject.FindWithTag("Player"));
-            Instantiate(_prefab, ResetPoint, Quaternion.identity);
-        }
+        if (_trigger.WasPerformedThisFrame()) ResetPlayer();
+    }
+
+    void ResetPlayer()
+    {
+        Destroy(GameObject.FindWithTag("Player"));
+        Instantiate(_prefab, ResetPoint, Quaternion.identity);
     }
 }
--- Assets/Scripts/TireController.cs
+++ Assets/Scripts/TireController.cs
@@ -1,22 +1,47 @@
 using UnityEngine;
 using UnityEngine.InputSystem;
+using UnityEngine.UIElements;
 
 public sealed class TireController : MonoBehaviour
 {
     [SerializeField] HingeJoint[] _joints = null;
     [SerializeField] InputAction _accelKey = null;
+    [SerializeField] string _uiButtonName = "button";
     [SerializeField] float _force = 500;
     [SerializeField] float _velocity = 500;
 
+    Button _button;
+    bool _pressed;
+
     void OnEnable()
-      => _accelKey.Enable();
+    {
+        _button = FindFirstObjectByType<UIDocument>()
+          .rootVisualElement.Q<Button>(_uiButtonName);
+
+        _accelKey.Enable();
+
+        _button.RegisterCallback<PointerDownEvent>(ButtonEvent, TrickleDown.TrickleDown);
+        _button.RegisterCallback<PointerUpEvent>(ButtonEvent, TrickleDown.TrickleDown);
+    }
 
     void OnDisable()
-      => _accelKey.Disable();
+    {
+        _accelKey.Disable();
+
+        _button.UnregisterCallback<PointerDownEvent>(ButtonEvent);
+        _button.UnregisterCallback<PointerUpEvent>(ButtonEvent);
+    }
+
+    void ButtonEvent<TEvent>(PointerEventBase<TEvent> evt)
+      where TEvent : PointerEventBase<TEvent>, new()
+    {
+        _pressed |=  (evt.eventTypeId == PointerDownEvent.TypeId());
+        _pressed &= !(evt.eventTypeId == PointerUpEvent.TypeId());
+    }
 
     void Update()
     {
-        var accel = _accelKey.ReadValue<float>();
+        var accel = _pressed ? 1.0f : _accelKey.ReadValue<float>();
 
         var motor = new JointMotor()
           { force = _force * accel, targetVelocity = _velocity * accel };
