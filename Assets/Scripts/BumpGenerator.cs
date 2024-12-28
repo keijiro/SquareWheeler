8d35d57c293cf3beba0a530606999091b2cd166a
Keijiro Takahashi <keijiro@gmail.com>
2024-12-28 14:36:23 +0900

    Difficulty tweaks

--- Assets/Scripts/BumpGenerator.cs
+++ Assets/Scripts/BumpGenerator.cs
@@ -6,31 +6,38 @@ public sealed class BumpGenerator : MonoBehaviour
 {
     [SerializeField] GameObject _prefab = null;
 
-    [field:SerializeField] public int Repeat { get; set; } = 20;
-    [field:SerializeField] public float3 Extent { get; set; } = 1;
-    [field:SerializeField] public float3 Rotation { get; set; } = 10;
-    [field:SerializeField] public uint Seed { get; set; } = 1234;
+    [field:SerializeField] public float2 Extent { get; set; }
+    [field:SerializeField] public int BumpCount { get; set; }
+    [field:SerializeField] public float BumpCountInc { get; set; }
+    [field:SerializeField] public float Height { get; set; }
+    [field:SerializeField] public float HeightInc { get; set; }
+    [field:SerializeField] public float Undulation { get; set; }
+    [field:SerializeField] public float UndulationInc { get; set; }
+    [field:SerializeField] public uint Seed { get; set; }
 
     void Start()
-      => GenerateBumps();
-
-    void GenerateBumps()
     {
-        var seed = Seed + (uint)transform.position.z;
-        var rand = new Random(seed);
-        rand.NextUInt4();
+        var prog = Mathf.RoundToInt(transform.position.z);
 
-        for (var i = 0; i < Repeat; i++)
+        var bumpCount = (int)(BumpCount + BumpCountInc * prog);
+        var height = Height + HeightInc * prog;
+        var undulation = Undulation + UndulationInc * prog;
+
+        var p_min = math.float3(-Extent.x, 0, -Extent.y);
+        var p_max = math.float3(Extent.x, height, Extent.y);
+
+        var r_min = math.radians(math.float3(-undulation, -180, -undulation));
+        var r_max = math.radians(math.float3( undulation,  180,  undulation));
+
+        var rand = new Random(Seed + (uint)prog);
+        rand.NextUInt4(); // warming up
+
+        for (var i = 0; i < bumpCount; i++)
         {
             var instance = Instantiate(_prefab, transform);
             var xform = instance.GetComponent<Transform>();
-
-            var p = rand.NextFloat3(-0.5f, 0.5f) * Extent;
-            var a = rand.NextFloat3(-0.5f, 0.5f) * Rotation;
-            var r = quaternion.Euler(math.radians(a));
-
-            xform.localPosition = p;
-            xform.localRotation = r;
+            xform.localPosition = rand.NextFloat3(p_min, p_max);
+            xform.localRotation = quaternion.Euler(rand.NextFloat3(r_min, r_max));
         }
     }
 }
