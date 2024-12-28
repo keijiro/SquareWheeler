8fff3cd5d80e440ec42605f6e05bdbfcc16d0be1
Keijiro Takahashi <keijiro@gmail.com>
2024-12-28 16:18:11 +0900

    Checkpoint

--- Assets/Scripts/Checkpoint.cs
+++ Assets/Scripts/Checkpoint.cs
@@ -1,15 +1,30 @@
 using UnityEngine;
+using UnityEngine.UIElements;
 
-public sealed class SegmentSpawner : MonoBehaviour
+public sealed class Checkpoint : MonoBehaviour
 {
     [SerializeField] GameObject _prefab = null;
     [SerializeField] Transform _spawnPoint = null;
     [SerializeField] float _stride = 50;
 
+    Label _label;
     bool _touched;
+
+    async void StartBlinkLabel()
+    {
+        for (var i = 0; i < 12; i++)
+        {
+            _label.visible ^= true;
+            await Awaitable.WaitForSecondsAsync(0.2f);
+        }
+    }
 
     void OnCollisionEnter(Collision collision)
       => _touched = true;
+
+    void Start()
+      => _label = FindFirstObjectByType<UIDocument>()
+                    .rootVisualElement.Q<Label>("checkpoint-label");
 
     void Update()
     {
@@ -17,6 +32,7 @@ public sealed class SegmentSpawner : MonoBehaviour
         {
             Instantiate(_prefab, _spawnPoint.position, Quaternion.identity);
             transform.position += Vector3.forward * _stride;
+            StartBlinkLabel();
             _touched = false;
         }
     }
